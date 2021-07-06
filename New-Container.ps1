137f60a8a69cef23f0bcbaef12975dd0132bd8f5
Aleksander Mavec <aleksander.mavec@be-terna.com>
2021-07-06 13:53:34 +0200

    Changed converting json to hashtable

--- New-Container.ps1
+++ New-Container.ps1
@@ -1,38 +1,9 @@
-# Before starting this script please update Manifest.json
+./Install/Update-BCContainerHelper.ps1
 
-Import-Module .\Install\Load-Configuration.psm1 -Force
+./Install/Install-BCContainer.ps1
 
-. (Join-Path $PSScriptRoot "install\Update-BCContainerHelper.ps1")
+./Install/Install-Certificate.ps1
 
-Write-Host ""
-$credential = Invoke-Credential
+./Install/Set-SessionTimeout.ps1
 
-$containerName = Get-ContainerName
-$artifactUrl = Get-ArtifactUrl
-$authentication = Get-Authentication
-$assignPremiumPlan = Get-AssignPremiumPlan
-$licenseFile = Get-LicenseFile
-
-Remove-BCContainer $containerName
-
-# https://github.com/microsoft/navcontainerhelper/blob/master/ContainerHandling/New-NavContainer.ps1
-
-New-BCContainer `
-    -accept_eula `
-    -containerName $containerName `
-    -artifactUrl $artifactUrl `
-    -Credential $credential `
-    -useSSL `
-    -auth $authentication `
-    -licenseFile $licenseFile `
-    -assignPremiumPlan:$assignPremiumPlan `
-    -updateHosts `
-    -accept_outdated `
-    -includeAL `
-    -memoryLimit 10G `
-    -isolation hyperv `
-    -shortcuts 'None'
-
-. (Join-Path $PSScriptRoot "Install\Install-Certificate.ps1")
-
-Start-Process "https://$containerName/BC/?tenant=default&signInRedirected=1"
+./Install/Invoke-WebClient.ps1
